34b5326c27125fc7b955ed54160b676a1b40f091
Adriana Ruiz <adriannaruiz24@gmail.com>
2022-12-20 17:24:42 -0500

    con navbar y agregando estilos css

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,6 +2,7 @@ import { faBars } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import { useState } from 'react'
+import { NavLink } from 'react-router-dom'
 import logo from '../assets/movieinfo-sinFondo.png'
 import Menu from './Menu'
 
@@ -24,6 +25,15 @@ const toOpenMenu = () => {
       <section className='header__section'>
         <img src={logo} alt="logo" className='header__logo'/>
         <p>Welcome to Movie Info</p>
+
+        <div className='navbar__container'>
+            <NavLink to={'/home'} className='header__navbarOptions active'>Home</NavLink>
+            <NavLink to={'/random'} className='header__navbarOptions active'>Random</NavLink>
+            <NavLink to={'/movies'} className='header__navbarOptions active'>Movies</NavLink>
+            <NavLink to={'/series'} className='header__navbarOptions active'>Series</NavLink>
+            <NavLink to={'/'} className='header__navbarOptions active'>Sign out</NavLink>
+        </div>
+
         <button 
           className='header__btnMenu'
           onClick={toOpenMenu}
