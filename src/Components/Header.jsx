bec02e2c435365ea389dd486ffda1cdf23969424
Adriana Ruiz <adriannaruiz24@gmail.com>
2022-12-20 20:09:20 -0500

    con estilos en los botones y navbar con el active en los links para mostrar donde esta parado el usuario

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,4 +1,4 @@
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faArrowRightFromBracket, faBars } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import { useState } from 'react'
@@ -6,9 +6,10 @@ import { NavLink } from 'react-router-dom'
 import logo from '../assets/movieinfo-sinFondo.png'
 import Menu from './Menu'
 
+<FontAwesomeIcon icon="fa-solid fa-arrow-right-from-bracket" />
+
 const Header = () => {
 
-  // const [menu, setMenu] = useState([])
   const [openMenu, setOpenMenu] = useState(false)
 
   const closeMenu = () => {
@@ -27,11 +28,11 @@ const toOpenMenu = () => {
         <p>Welcome to Movie Info</p>
 
         <div className='navbar__container'>
-            <NavLink to={'/home'} className='header__navbarOptions active'>Home</NavLink>
-            <NavLink to={'/random'} className='header__navbarOptions active'>Random</NavLink>
-            <NavLink to={'/movies'} className='header__navbarOptions active'>Movies</NavLink>
-            <NavLink to={'/series'} className='header__navbarOptions active'>Series</NavLink>
-            <NavLink to={'/'} className='header__navbarOptions active'>Sign out</NavLink>
+            <NavLink to={'/home'} className='header__navbarOptions' activeclassname='active'>Home</NavLink>
+            <NavLink to={'/random'} className='header__navbarOptions' activeclassname='active' >Random</NavLink>
+            <NavLink to={'/movies'} className='header__navbarOptions' activeclassname='active'>Movies</NavLink>
+            <NavLink to={'/series'} className='header__navbarOptions' activeclassname='active'>Series</NavLink>
+            <NavLink to={'/'} className='header__navbarOptions signOutIcon' activeclassname='active'><FontAwesomeIcon icon={faArrowRightFromBracket}/></NavLink>
         </div>
 
         <button 
